08444a254043700a387827c43dba253d1aed7909
bishal094 <48466664+bishal094@users.noreply.github.com>
2024-08-07 15:26:25 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NTLKHWLK');</script>
+<!-- End Google Tag Manager -->
         <title>Create Wix Education Site</title>
         <meta
           name="description"
@@ -25,6 +32,10 @@ export default function RootLayout({
         <link rel="icon" href="https://www.wix.com/favicon.ico" />
       </head>
       <body className="text-blue-site bg-white">
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTLKHWLK"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         {process.env.NEXT_PUBLIC_WIX_CLIENT_ID ? (
           <>
             <Header />
